57306a114ca88acf11ec7637c8dc2998b74c2e09
ZHONGCHENG ZHAO <zhongchengzhao@ZHONGCHENGs-MBP.lan>
2023-02-15 10:16:03 -0600

    deploy

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,11 +16,9 @@ function About() {
           <div className="col-6">
             <h1 id="about">About me</h1>
             <h6 style={{ width: "80%", justifyContent: "center" }} id="about">
-              TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest
+              My name is Zhongcheng Zhao, and I can go by Zack. And I'm a full-stack software engineer. I have my Bachelor's Degrees in Finance from University of Kansas. Also I always have passion for computer science, especiaaly coding. I had been working several projects in KU bootcamp likes Note taker, weather dashboard and work day schedule. 
               <br></br>
-              <br></br>TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest
-              TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest
-              TestTestTestTestTestTestTestTestTestTestTestTest
+              <br></br>While I am in the bootcamp, I had learned how to use different skills for solving problem. 
             </h6>
             </div>
       </div>
